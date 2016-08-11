4173f7f5de05e10d177b22f137e5e1ab32c1a484
admin <lovychen@126.com>
2016-08-11 11:24:25 +0800

    添加分词

--- start_ttt.py
+++ start_ttt.py
@@ -7,5 +7,34 @@ import os
 import codecs
 from sklearn import feature_extraction
 import mpld3
-# stopwords = nltk.corpus.stopwords.words('stop_words.txt')
-# print stopwords[:10]
+stopwords = nltk.corpus.stopwords.words('english')
+print stopwords[:10]
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+print stemmer.text
+
+def tokenize_and_stem(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
+
+
+def tokenize_only(text):
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    return filtered_tokens
+
+
+if __name__ ="__main__":
+    tokenize_only()
